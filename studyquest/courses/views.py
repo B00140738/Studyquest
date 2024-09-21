54facc84b3f5a6ff5a0b0eafbfab9d31cd734f51
B00140738 <B00140738@mytudublin.ie>
2024-09-21 14:16:31 +0100

    added base routes
    added urls.py to route folders along with  index methods in views.py

--- studyquest/courses/views.py
+++ studyquest/courses/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# Index page
+def index(request):
+    return HttpResponse("Hello World. You're the the courses index")
--- studyquest/study_sessions/views.py
+++ studyquest/study_sessions/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# Index Page
+def index(request):
+    return HttpResponse("Hello, world. You're at the Study Sessions index")
